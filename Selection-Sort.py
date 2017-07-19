2495ee20e9958d2ae09d8a7788daf2595227462a
FatefulStudent <30292514+FatefulStudent@users.noreply.github.com>
2017-07-19 21:27:38 +0300

    Shortened Selection-Sort
    Redid initialization to include negative numbers and be in just one line

--- Selection-Sort.py
+++ Selection-Sort.py
@@ -1,15 +1,12 @@
 from random import randint
-#Initializing the list by appending to an empty list
-#100 Random numbers
-numbers = []
-for value in range (0, 100):
-    numbers.append(randint(1, 1000))
+#Initializing a list of 100 Random numbers
+numbers = [randint(-100, 100) for value in range(1,10)]
 
 #Sort itself. It is one of the most simple ones
 #Complexity: O(n^2)
     
 sorted_numbers = [] #soon-to-be sorted list
-maxint = 0 
+maxint = float('-inf')
 
 for turn in range (0, len(numbers)):
     for number in numbers:
@@ -17,7 +14,7 @@ for turn in range (0, len(numbers)):
             maxint = number
     sorted_numbers.append(maxint)
     numbers.remove(maxint)
-    maxint = 0
+    maxint = float('-inf')
     
 print("Descending Order:")
 print(sorted_numbers) 
